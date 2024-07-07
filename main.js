c5c81d7f5fdfb52e1208e2fa0ac133f8fc78d6b9
Bong <borobong230@gmail.com>
2024-07-07 17:20:55 +0900

    :hammer: [Update] import Router.js at main.js #main

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hello World from main.js!");
+import Router from "./Router.js";
+
+const routes = {
+  "/": { template: "<h1>Home</h1><p>Welcome to the Home Page</p>" },
+  "/about": { template: "<h1>About</h1><p>About us page</p>" },
+};
+
+const router = new Router(routes);
