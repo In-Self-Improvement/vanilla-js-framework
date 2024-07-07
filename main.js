07de677f706b84a6566324ed414e64a4678587f4
Bong <borobong230@gmail.com>
2024-07-07 17:23:41 +0900

    :hammer: [Update] Add component.js at main.js #main

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import Router from "./Router.js";
+import Component from "./Component.js";
+
+class HomeComponent extends Component {
+  render() {
+    return `<h1>Home</h1><p>Welcome, ${this.props.name}!</p>`;
+  }
+}
 
 const routes = {
-  "/": { template: "<h1>Home</h1><p>Welcome to the Home Page</p>" },
+  "/": { template: new HomeComponent({ name: "User" }).render() },
   "/about": { template: "<h1>About</h1><p>About us page</p>" },
 };
 
