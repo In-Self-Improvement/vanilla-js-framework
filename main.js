af2b19ce54066bec0c2b5dadb7748e28690745df
Bong <borobong230@gmail.com>
2024-07-07 17:24:26 +0900

    :hammer: [Update] Add State.js at main.js #main

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 import Router from "./Router.js";
 import Component from "./Component.js";
+import State from "./State.js";
 
-class HomeComponent extends Component {
+const state = new State({ count: 0 });
+
+class CounterComponent extends Component {
+  constructor(props) {
+    super(props);
+    state.subscribe(this.render.bind(this));
+  }
+
+  increase() {
+    state.setState({ count: state.getState().count + 1 });
+  }
+
+  decrease() {
+    state.setState({ count: state.getState().count - 1 });
+  }
+
   render() {
-    return `<h1>Home</h1><p>Welcome, ${this.props.name}!</p>`;
+    return `
+      <h1>Counter</h1>
+      <p>Count: ${state.getState().count}</p>
+      <button onclick="counter.decrease()">-</button>
+      <button onclick="counter.increase()">+</button>
+    `;
   }
 }
 
+const counter = new CounterComponent();
+
 const routes = {
-  "/": { template: new HomeComponent({ name: "User" }).render() },
+  "/": { template: counter.render() },
   "/about": { template: "<h1>About</h1><p>About us page</p>" },
 };
 
