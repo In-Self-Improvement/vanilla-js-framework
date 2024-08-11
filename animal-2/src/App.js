6d2125218ee165b69f326e24b6e6ffb7268b08bf
Bong <borobong230@gmail.com>
2024-08-11 21:47:51 +0900

    :sparkles: [Feat] App component to use TabBar and Content components with updated state management #16

--- animal-2/src/App.js
+++ animal-2/src/App.js
@@ -1,7 +1,41 @@
 import TabBar from "./components/TabBar";
 import Content from "./components/Content";
+import { request } from "./components/api.js";
 
 export default function App() {
-  const tabBar = new TabBar();
+  this.state = {
+    currentTab: "all",
+    photos: [],
+  };
+
+  const tabBar = new TabBar({
+    initialState: "",
+    onClick: async (name) => {
+      this.setState({
+        ...this.state,
+        currentTab: name,
+        photos: await request(name),
+      });
+    },
+  });
   const content = new Content();
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    tabBar.setState(this.state.currentTab);
+    content.setState(this.state.photos);
+  };
+
+  const init = async () => {
+    try {
+      const initialPhotos = await request();
+      this.setState({
+        ...this.state,
+        photos: initialPhotos,
+      });
+    } catch (err) {
+      console.log("init error", err);
+    }
+  };
+  init();
 }
